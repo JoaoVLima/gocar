86e465c1dfccb3e95983974c4459aae835a4836f
droquee3 <caiors3347@gmail.com>
2025-08-27 11:33:50 -0300

    Adição de menu inicial

--- src/gocar/Main.java
+++ src/gocar/Main.java
@@ -1,7 +1,9 @@
 package gocar;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("--- GoCar Loja de carros e Mecanica  ---");
         System.out.println();
@@ -13,8 +15,54 @@ public class Main {
         Carro carro2 = new Carro("Fiat", "Uno", 2015, 40000, 40, 15, 2, true);
         Carro carro3 = new Carro("Honda", "Civic", 2018, 80000, 50, 20, 4, true);
 
-        carro1.exibirInfo();
-        carro2.exibirInfo();
-        carro3.exibirInfo();
+        boolean rodando = true;
+
+        while (rodando) {
+            System.out.println("\n--- GoCar Loja de Carros ---");
+            System.out.println("1 - Ver modelos disponíveis");
+            System.out.println("2 - Selecionar marca e ver detalhes");
+            System.out.println("3 - Fechar aplicativo");
+            System.out.print("Escolha uma opção: ");
+
+            int opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("\nModelos disponíveis:");
+                    System.out.println("- " + carro1.getMarca() + " " + carro1.getModelo());
+                    System.out.println("- " + carro2.getMarca() + " " + carro2.getModelo());
+                    System.out.println("- " + carro3.getMarca() + " " + carro3.getModelo());
+                    break;
+
+                case 2:
+                    System.out.println("\nEscolha a marca:");
+                    System.out.println("1 - Toyota");
+                    System.out.println("2 - Fiat");
+                    System.out.println("3 - Honda");
+                    System.out.println("4 - Voltar");
+                    System.out.print("Digite a opção: ");
+                    int escolha = sc.nextInt();
+
+                    switch (escolha) {
+                        case 1: carro1.exibirInfo(); break;
+                        case 2: carro2.exibirInfo(); break;
+                        case 3: carro3.exibirInfo(); break;
+                        case 4: break; // volta pro menu
+                        default: System.out.println("Opção inválida!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Encerrando aplicativo...");
+                    rodando = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+
+        sc.close();
     }
 }
